9329d6d51264809876dc0a2947ba67faba3f446f
Noah <njbotkin@gmail.com>
2018-02-15 22:13:06 -0600

    eslint-formatter

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
-const diff = require('diff')
-const chalk = require('chalk')
-const stripAnsi = require('strip-ansi')
+const diff = require(`diff`)
+const chalk = require(`chalk`)
+const stripAnsi = require(`strip-ansi`)
 
 const terminalWidth = (process.stdout && process.stdout.columns) ? process.stdout.columns : 80
 
-var colwidth = Math.floor((terminalWidth - 6) / 2)
+const colwidth = Math.floor((terminalWidth - 6) / 2)
 
 function fit(str, width) {
-	var strProcessed = ''
-	var center = width/2
+	let strProcessed = ``
+	const center = width / 2
 
-	if(stripAnsi(str).length > width) {
+	if (stripAnsi(str).length > width) {
 		// truncate middle (this is buggy. strip out part of an ANSI escape code and stuff breaks)
-		var overflow = str.length - width + 1
-		var remove = str.substr( center, overflow )
-		strProcessed = str.replace(remove, '…')
+		const overflow = str.length - width + 1
+		const remove = str.substr(center, overflow)
+		strProcessed = str.replace(remove, `…`)
 	} else {
 		// right-pad
 		strProcessed = str
-		var s = width - stripAnsi(str).length
-		for(s; s > 0; s--) strProcessed += ' '
+		let s = width - stripAnsi(str).length
+		for (s; s > 0; s--) {
+			strProcessed += ` `
+		}
 	}
 	return strProcessed
 }
 
 module.exports = {
-	splitdiff(one, two, { diffType = 'diffChars'}) {
+	splitdiff(one, two, { diffType = `diffChars` } = {}) {
+		if (diffType !== `diffChars` && diffType !== `diffLines`) {
+			throw (`unsupported diffType!`)
+		}
 
-		if(diffType !== 'diffChars' && diffType !== 'diffLines') throw('unsupported diffType!')
-
-		var left = ''
-		var right = ''
+		let left = ``
+		let right = ``
 
 		function alignLines() {
-			while((left.match(/\n/g) || []).length < (right.match(/\n/g) || []).length) left += '\n'
-			while((left.match(/\n/g) || []).length > (right.match(/\n/g) || []).length) right += '\n'
+			while ((left.match(/\n/g) || []).length < (right.match(/\n/g) || []).length) {
+				left += `\n`
+			}
+			while ((left.match(/\n/g) || []).length > (right.match(/\n/g) || []).length) {
+				right += `\n`
+			}
 		}
 
 		diff[diffType](one, two).forEach(e => {
-			if(!e.added && !e.removed) {
+			if (!e.added && !e.removed) {
 				alignLines()
 
 				left += e.value
 				right += e.value
 			}
-			if(e.removed) {
+			if (e.removed) {
 				left += chalk.bold.red(e.value)
 			}
-			if(e.added) {
+			if (e.added) {
 				right += chalk.bold.green(e.value)
 			}
 		})
@@ -55,14 +62,14 @@ module.exports = {
 		alignLines()
 
 		// draw
-		var leftLines = left.split('\n')
-		var rightLines = right.split('\n')
-		var output = ''
+		const leftLines = left.split(`\n`)
+		const rightLines = right.split(`\n`)
+		let output = ``
 
-		for(var i = 0; i < leftLines.length; i++) {
-			output += fit(leftLines[i], colwidth) + '  '+fit(rightLines[i], colwidth)
+		for (let i = 0; i < leftLines.length; i++) {
+			output += fit(leftLines[i], colwidth) + `  ` + fit(rightLines[i], colwidth) + `\n`
 		}
 
 		return output
-	}
+	},
 }
